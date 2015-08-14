acc91061e21840347610cfc7e39c74e3fe809792
spacetime <spacetime@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-14 20:23:00 +0000

    Tagging 1.5.6
    git-svn-id: https://plugins.svn.wordpress.org/ad-inserter/tags/1.5.6@1221216 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta http-equiv="content-style-type" content="text/css" />
+        <meta http-equiv="content-script-type" content="text/javascript" />
+
+        <title>Ad Inserter - Adsense and Amazon ads for Wordpress</title>
+        <meta name="description" content="Ad Inserter - Insert Adsense, Amazon or any other code into Wordpress" />
+        <meta name="keywords" content="ad inserter, wordpress, adsense, amazon, html, code, javascript, php" />
+
+        <style>
+        body {
+          font-family: arial, helvetica;
+        }
+        </style>
+    </head>
+
+    <body>
+      <div style="float: left;">
+        <div style="width: 800px; padding: 2px 8px 6px 8px; margin: 8px 30px 8px 0; border: 1px solid rgb(221, 221, 221); border-radius: 5px; background: #fff;">
+          <h1 style="text-align: center;">Ad Inserter</h1>
+          <p>
+            A simple solution to insert any code into Wordpress. Perfect for AdSense or contextual Amazon ads. Simply enter any HTML/Javascript/PHP code and select where and how you want to display it.
+          </p>
+          <div>
+            <img style="display: block;margin: 0 auto;" src="screenshot-1.jpg" />
+          </div>
+          <p>
+            Settings for one code block. Up to 16 blocks can be configured.
+          </p>
+          <hr />
+          <div>
+            <img style="display: block;margin: 0 auto;" src="screenshot-2.jpg" />
+          </div>
+          <p>
+            Settings for header code
+          </p>
+          <hr />
+          <div>
+            <img style="display: block;margin: 0 auto;" src="screenshot-3.jpg" />
+          </div>
+          <p>
+            Ad Inserter settings
+          </p>
+          <hr />
+          <div>
+            <img style="display: block;margin: 0 auto;" src="screenshot-4.jpg" width="800"/>
+          </div>
+          <p>
+            Post / Page Ad Inserter Exceptions
+          </p>
+        </div>
+      </div>
+
+      <div style="float: left;">
+        <div style="width: 800px; padding: 2px 8px 6px 8px; margin: 8px 0 8px 0; border: 1px solid rgb(221, 221, 221); border-radius: 5px; background: #fff;">
+          <h1 style="text-align: center;">Tiny Monitor</h1>
+          <h2 style="text-align: center;">Monitor Google AdSense and Amazon Associates earnings</h2>
+          <p style="text-align: justify;">
+            Tiny Monitor for
+            <a href="http://tinymonitor.com/adsense-monitor-documentation" target="_blank">AdSense</a>,
+            <a href="http://tinymonitor.com/amazon-monitor-documentation" target="_blank">Amazon</a> and
+            <a href="http://tinymonitor.com/paypal-monitor-documentation" target="_blank">PayPal</a> is a perfect solution to track your online earnings.
+            This is a small PHP script that can be installed on any <a href="http://tinymonitor.com/installing-web-server" target="_blank">home web server</a> or commercial web hosting.
+            If you already have some website (very likely as you are using Ad Inserter plugin) then you can simply put it into a folder.
+          </p>
+          <p style="text-align: justify;">
+            Tiny Monitor automatically downloads data from Google, Amazon or PayPal and presents it in a simple single-page form.
+            This way you can have all the data easily accessible without the need to log into many websites just to check today's earnings.
+          </p>
+          <a href="http://tinymonitor.com/" target="_blank"><img style="display: block;margin: 0 auto;" src="images/tinymonitor-logo.png" alt="Tiny Monitor"></a>
+          <a href="http://tinymonitor.com/" target="_blank"><img style="display: block;margin: 0 auto;" src="images/tiny-monitor.png" alt="Amazon Associates"></a>
+          <p style="text-align: justify;">
+            And if you have more than one domain then you can monitor them with <a href="http://tinymonitor.com/domain-monitor-documentation" target="_blank">Tiny Domain Monitor</a>.
+            This PHP script is a simple yet effective solution to track expiration dates of your own domains or any other domains you would like to register as soon as they expire.
+          </p>
+          <a href="http://tinymonitor.com/domainmonitor/" target="_blank"><img style="display: block;margin: 0 auto;" src="images/domain-monitor.png" alt="Domain Monitor Script"></a>
+        </div>
+      </div>
+    <body>
+
+</html>
+
